1889112238c69b3f7c7ce07959651ec703a557a1
Mikayel <mikael.bejanyan@gmail.com>
2022-06-10 19:22:57 +0400

    feat: server: status-services deleteStatus added

--- src/modules/statuses/services.js
+++ src/modules/statuses/services.js
@@ -10,6 +10,7 @@ import {
 export const getAllStatuses = async (req, res, next) => {
   try {
     const allStatuses = await getAllStatusesDB()
+
     res.json(responseDataCreator(allStatuses))
   } catch (error) {
     next(error)
@@ -21,6 +22,7 @@ export const getAllStatuses = async (req, res, next) => {
 export const createStatus = async (req, res, next) => {
   try {
     const newStatus = await createStatusDB(req.body)
+
     res.json(responseDataCreator(newStatus))
   } catch (error) {
     next(error)
@@ -44,6 +46,17 @@ export const updateStatus = async (req, res, next) => {
     const updatedStatus = await updateStatusDB(req.body, req.params.id)
 
     res.json(responseDataCreator(updateStatus))
+  } catch (error) {
+    next(error)
+  }
+}
+
+// delete
+export const deleteStatus = async (req, res, next) => {
+  try {
+    const deletedStatus = await deleteStatusDB(req.params.id)
+
+    res.json(responseDataCreator(deleteStatus))
   } catch (error) {
     next(error)
   }
