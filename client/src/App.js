e665abf83dcc44fa4f6708c5c51606a1cb148dd2
armeagle777 <tyeranyan@gmail.com>
2022-07-06 21:01:41 +0400

    lock.json modified

--- client/src/App.js
+++ client/src/App.js
@@ -3,38 +3,6 @@ import Signup from "./Pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { AuthProvider } from "./components/auth.js";
-<<<<<<< HEAD
-import Profile from "./Pages/Profile.jsx";
-import RequireAuth from "./components/RequireAuth";
-import Employee from "./Pages/Employee";
-import GuestPage from "./Pages/GuestPage"
-
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="login" element={<Login type="login" />} />
-          <Route path="signup" element={<Signup type="signup" />} />
-          <Route path="*" element={<Navigate to="signup" />} />
-          <Route path="employee" element={<Employee />} />
-          <Route path='guest' element={<GuestPage />} />
-          <Route path="users">
-            <Route path=":userId" element={<Login />} />
-            <Route path="admin" element={<Login />} />
-            <Route
-              path="profile"
-              element={
-                <RequireAuth>
-                  <Profile />
-                </RequireAuth>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
-=======
 import Profile from "./Pages/Profile";
 import RequireAuth from "./components/RequireAuth"
 import AdminHome from "./Pages/AdminHome";
@@ -77,7 +45,6 @@ function App() {
                                 </UsersContextProvider>
                           </AuthProvider>
 
->>>>>>> develop
   );
 
         
